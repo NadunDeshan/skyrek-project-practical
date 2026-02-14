4812645e87f7e428c2579ae86c4c3a8da5d782f1
Nadun <nadundeshan16gmail.com>
2026-02-14 09:04:27 +0530

    is cusomer function created for validations❤️

--- controllers/userController.js
+++ controllers/userController.js
@@ -79,3 +79,13 @@ export function isAdmin(req){
     }
     return true;
 }
+
+export function isCustomer(req){
+    if(req.user == null){
+        return false;
+    }
+    if(req.user.role !="user"){
+        return false;
+    }
+    return true;
+}
