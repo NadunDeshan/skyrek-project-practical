a50cea2ce86c2a1d98095e446fc9b4d5b901ce80
Nadun <nadundeshan16gmail.com>
2026-03-06 19:27:25 +0530

    Fix email app password env name

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,7 +13,7 @@ const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    pass: process.env.APP_PASSWORD,
   },
 });
 
